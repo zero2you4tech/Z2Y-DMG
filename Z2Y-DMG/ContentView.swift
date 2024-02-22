c096972d0559d778f51ddceb7abed2dd5343f896
欧阳Boy_Petter <1460300366@qq.com>
2024-02-22 14:10:01 +0800

    Initial Commit

---
+++ Z2Y-DMG/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Z2Y-DMG
+//
+//  Created by Oyjie on 2/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Z2Y-DMG/Z2Y_DMGApp.swift
@@ -0,0 +1,17 @@
+//
+//  Z2Y_DMGApp.swift
+//  Z2Y-DMG
+//
+//  Created by Oyjie on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Z2Y_DMGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
